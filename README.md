91849efb0fccc70e1725bf7a44c25d7d7dd6a6a7
Arkaraj <arkaraj2017@gmail.com>
2021-06-13 23:19:13 +0530

    Iniitialized ts-graphql

---
+++ README.md
@@ -0,0 +1,19 @@
+# Galahad
+
+Graphql api with Typescript
+
+## Tech Stack
+
+- TypeScript
+- GraphQL
+- Sql DB (Mysql)
+- Express, Typeorm
+
+## Ts Graphql Setup
+
+```bash
+
+# cmd from https://github.com/benawad/create-graphql-api
+npx create-graphql-api Galahad
+
+```
---
+++ ormconfig.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    type: "mysql",
+    port: 3306,
+    username: `${process.env.SQL_Username}`,
+    password: `${process.env.SQL_Password}`,
+    database: `${process.env.SQL_Database}`,
+    synchronize: true,
+    logging: true,
+    entities: ["src/entity/**/*.ts"],
+    migrations: ["src/migration/**/*.ts"],
+    subscribers: ["src/subscriber/**/*.ts"],
+    cli: {
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migration",
+      subscribersDir: "src/subscriber",
+    },
+  },
+  {
+    type: "mysql",
+    url: process.env.DATABASE_URL,
+    synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
+    logging: false,
+    entities: ["dist/entity/**/*.js"],
+    migrations: ["dist/migration/**/*.js"],
+    subscribers: ["dist/subscriber/**/*.js"],
+    cli: {
+      entitiesDir: "dist/entity",
+      migrationsDir: "dist/migration",
+      subscribersDir: "dist/subscriber",
+    },
+  },
+];
